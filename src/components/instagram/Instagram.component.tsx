d85f8a2a1253985c7405afd6572bc83c9b1b40e3
Brennan Walsh <mail@brennanwal.sh>
2021-02-16 16:37:30 -0800

    Instagram component timestamp implementation.

--- src/components/instagram/Instagram.component.tsx
+++ src/components/instagram/Instagram.component.tsx
@@ -3,27 +3,36 @@ import {getMediaByUsername} from 'nanogram.js'
 import {StyledInstagram} from '.'
 
 export const Instagram: App.InstagramComponent = ({styles, className}) => {
-	const [igFeed, setIgFeed] = useState<string[]>([])
+	const [igFeed, setIgFeed] = useState<{feed?: string[]; time?: number}>({
+		feed: [],
+		time: 0
+	})
 
 	useEffect(() => {
 		const feed = JSON.parse(localStorage.getItem('feed') as string)
-		if (feed) {
+		const time = Date.now()
+		if (feed && time - feed.time < 86400000) {
 			setIgFeed(feed)
 		} else {
-			setIgFeed([])
+			console.log('Pulling IG images from cache.')
+			setIgFeed({
+				feed: [],
+				time: 0
+			})
 			getMediaByUsername('iambrennanwalsh').then(media => {
 				let images = media!.profile!.edge_owner_to_timeline_media.edges!
 				images = images.splice(0, 10)
 				const items = images.map(img => img.node.thumbnail_resources[4].src)
-				setIgFeed(items)
-				localStorage.setItem('feed', JSON.stringify(items))
+				const feed = {feed: items, time: time}
+				setIgFeed(feed)
+				localStorage.setItem('feed', JSON.stringify(feed))
 			})
 		}
 	}, [])
 
 	return (
 		<StyledInstagram {...(styles && {css: styles})} {...(className && {className: className})}>
-			{igFeed.map(img => (
+			{igFeed.feed.map(img => (
 				<div key={img} style={{backgroundImage: `url(${img})`}}></div>
 			))}
 		</StyledInstagram>
