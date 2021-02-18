a6a4c376995800a2121f1e3cba090f6c10cb661d
Brennan Walsh <mail@brennanwal.sh>
2021-02-17 21:16:16 -0800

    Fix for production Map bug,

--- src/components/map/Map.component.tsx
+++ src/components/map/Map.component.tsx
@@ -1,30 +1,31 @@
 import {useEffect, useContext} from 'react'
-import 'leaflet/dist/leaflet.css'
-import {map, icon, marker, tileLayer} from 'leaflet'
-import {StyledMap} from '.'
 import {ComponentsContext} from '@/contexts/components'
+import {StyledMap} from '.'
+import {map, icon, marker, tileLayer} from 'leaflet'
+import 'leaflet/dist/leaflet.css'
 
 export const Map: App.MapComponent = ({className, styles}) => {
 	const context = useContext(ComponentsContext)
+
 	useEffect(() => {
-		const {key} = context
-		context.key = ''
-		const position: L.LatLngExpression = [33.8358, -118.3406]
-		const Map = map('map').setView(position, 9)
-		const Icon = icon({...icon, iconUrl: '/images/interface/marker.png'})
-		tileLayer(
-			'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
-			{
-				maxZoom: 9,
-				minZoom: 9,
-				id: 'iambrennanwalsh/ckd4c2rku0wdu1ip4cogwrzuu',
-				tileSize: 512,
-				zoomOffset: -1,
-				accessToken: key
-			}
-		).addTo(Map)
-		const Marker = marker(position, {icon: Icon}).addTo(Map)
-		Marker.bindPopup('<b>Torrance, CA</b><br />Born and raised.').openPopup()
+		if (typeof window != 'undefined') {
+			const position: L.LatLngExpression = [33.8358, -118.3406]
+			const Map = map('map').setView(position, 9)
+			const Icon = icon({...icon, iconUrl: '/images/interface/marker.png'})
+			const Marker = marker(position, {icon: Icon}).addTo(Map)
+			Marker.bindPopup('<b>Torrance, CA</b><br />Born and raised.').openPopup()
+			tileLayer(
+				'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
+				{
+					maxZoom: 9,
+					minZoom: 9,
+					id: 'iambrennanwalsh/ckd4c2rku0wdu1ip4cogwrzuu',
+					tileSize: 512,
+					zoomOffset: -1,
+					accessToken: context.key
+				}
+			).addTo(Map)
+		}
 	}, [])
 
 	return (
