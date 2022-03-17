4f32ecccb8a9297388678870e5f4728639a0c6e2
Brennan Walsh <mail@brennanwal.sh>
2022-03-17 01:27:21 -0700

    .

--- src/partials/main/Main.tsx
+++ src/partials/main/Main.tsx
@@ -1,14 +1,30 @@
-import type {Component, Props} from '@/'
-import {Content, Sidebar} from '@/partials'
-import {StyledMain} from '.'
+import type { Component, Props } from '@/'
+import { Content, Sidebar } from '@/partials'
+import Script from 'next/script'
+import { StyledMain } from '.'
 
 export type MainProps = Props<typeof StyledMain>
 
-export const Main: Component<MainProps> = ({children, ...props}) => {
+export const Main: Component<MainProps> = ({ children, ...props }) => {
   return (
-    <StyledMain id="main" {...props}>
-      <Content>{children}</Content>
-      <Sidebar />
-    </StyledMain>
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-CYC34DR7C4');
+        `}
+      </Script>
+      <StyledMain id="main" {...props}>
+        <Content>{children}</Content>
+        <Sidebar />
+      </StyledMain>
+    </>
   )
 }
